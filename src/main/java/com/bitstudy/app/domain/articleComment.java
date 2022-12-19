b3fd6e42f690eada9ebdc9a25a53a1e5c6b057e2
me3est <me3est@gmail.com>
2022-12-19 18:48:28 +0900

    zz

---
+++ src/main/java/com/bitstudy/app/domain/articleComment.java
@@ -0,0 +1,19 @@
+package com.bitstudy.app.domain;
+
+import java.time.LocalDateTime;
+
+public class articleComment {
+
+    private Long id;
+
+//    private Article article;
+
+    private String content; // 본문
+
+    // 메타데이터
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 생성일시
+    private String modifiedBy; // 수정자
+    
+}
