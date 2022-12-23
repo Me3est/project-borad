0db7c31bac0c7deae559a320427c4d7481d554a3
me3est <me3est@gmail.com>
2022-12-23 18:59:10 +0900

    dd

--- src/main/java/com/bitstudy/app/repository/ArticleRepository.java
+++ src/main/java/com/bitstudy/app/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.bitstudy.app.repository;
 
 import com.bitstudy.app.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /** TDD를 위해서 임시로 만들어놓은 저장소 (이거로 DB에 접근할거다)
  *
@@ -10,5 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2) JUint5 버전인지 확인
  * 3) 이름 ArticleRepositoryTest 를 JpaRepositoryTest 로 변경
  * */
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
